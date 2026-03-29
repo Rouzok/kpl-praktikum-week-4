29c246f49fe1a86569547be0803db6c4cec26165
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-03-29 18:28:31 +0700

    menambahkan class DataGeneric

--- tpmodul5_103082400009/Program.cs
+++ tpmodul5_103082400009/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TugasGeneric
+{
+    public class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myNIM = "103082400009";
+            DataGeneric<string> dataObj = new DataGeneric<string>(myNIM);
+
+            dataObj.PrintData();
+        }
+    }
+}
